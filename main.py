f485b1d1717a451bd9554f5e936c89f8bbe6d88a
Konstantinos Leimonis <k.leimonis@gmail.com>
2019-01-30 19:54:39 +0000

    Add lyrics print function

--- main.py
+++ main.py
@@ -28,16 +28,19 @@ def extract_lyrics_lines(lyrics_list):
         all_lyrics_list = all_lyrics_list + [line for line in lyrics.split('\n') if line]
     return all_lyrics_list
 
+def randomly_print_lyrics(lyrics):
+    for i in range(20):
+        index = random.randint(1,len(lyrics))
+        print(lyrics[index])
+        if i % 5 == 0:
+            print('\n', end='')
+
 def main():
     lyrics_list = get_lyrics()
     all_lyrics_lines = extract_lyrics_lines(lyrics_list)
     random.shuffle(all_lyrics_lines)
+    randomly_print_lyrics(all_lyrics_lines)
 
-    for i in range(20):
-        index = random.randint(1,len(all_lyrics_lines))
-        print(all_lyrics_lines[index])
-        if i % 5 == 0:
-            print('\n', end='')
 
 if __name__== "__main__":
   main()
